2a618bf17e6e3421ea0cb494ba9d8e44dfe84edd
borisngong <borisngong@gmail>
2024-10-10 13:55:56 -0700

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that takes a float and return a string rep
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that returns str represenation of a float
+    """
+    return str(n)
