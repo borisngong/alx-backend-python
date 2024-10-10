a3799df322a552d39384a45297605cce2691f581
borisngong <borisngong@gmail>
2024-10-10 16:56:11 -0700

    TypeVar annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 """
-A mofule for working with Any, Mapping, TypeVar, Union in typing
+A module for working with Any, Mapping, TypeVar, Union in typing.
 """
 from typing import Any, Mapping, TypeVar, Union
 
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping[Any, Any], key: Any,
-                     default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(
+    dct: Mapping[Any, Any],
+    key: Any,
+    default: Union[T, None] = None
+) -> Union[Any, T]:
     """Safely gets a value from a dictionary and returns key-value
-    if present and default if not
+    if present and default if not.
     """
     if key in dct:
         return dct[key]
