40fcb07d8f2f0331dfc471697c30176b15647990
borisngong <borisngong@gmail>
2024-10-10 13:12:52 -0700

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+"""
+This script contains a function to add two floating-point numbers.
+"""
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
     return a + b
