a4b91ba8996e286d23b18cf94421430dea669a8e
borisngong <borisngong@gmail>
2024-10-10 13:44:24 -0700

    Function that floors a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module responsible for returning the floor of a float
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returning the floor of a float
+    """
+    return math.floor(n)
