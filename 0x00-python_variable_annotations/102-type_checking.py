1a07b07098e26716e2dca29baf1663c8bef77b42
borisngong <borisngong@gmail>
2024-10-10 17:45:27 -0700

    Using mypy to validate pieces of code

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-""" A module for working with mypy to validate pieces of code
-and xooming in on elements by repeating over several times
 """
-from typing import List, Union, Tuple
+Module for working with mypy and zoomng in on arrays
+"""
+from typing import List, Tuple
 
 
-def zoom_array(lst: Union[List[int], Tuple[int, ...]], factor: int = 2) -> List[int]:
-    """Zooms in on an array by repeating elements based on the factor"""
-    zoomed_in: List[int] = [
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """
+    Zoom in on an array by repeating each element certain
+    number of times
+    """
+    zoomed_in: List = [
         item for item in lst
-        for _ in range(factor)
+        for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
-
+array = (12, 72, 91)
 zoom_2x = zoom_array(array)
-
 zoom_3x = zoom_array(array, 3)
