ae51c2b03afacdb8ce5c76f2d3286d87add992a1
borisngong <borisngong@gmail>
2024-10-14 17:46:26 -0700

    Cresate Coroutine Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,14 +4,14 @@ Module for altering wait_n to use task_wait_random instead
 """
 import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
+from typing import List
 
 
 async def task_wait_n(n: int, max_delay: int) -> list[float]:
     """
     Return the list of all the delays (float) in ascending order.
     """
-    tasks = [task_wait_random(max_delay) for _ in range(n)]
-
-    delays = await asyncio.gather(*tasks)
-
-    return sorted(delays)
+    delay_n = await asyncio.gather(
+        *tuple(map(lambda _: task_wait_random(max_delay), range(n)))
+    )
+    return sorted(delay_n)
