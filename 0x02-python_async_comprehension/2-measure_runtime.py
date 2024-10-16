c5c5c700c3f3e6f7ca2bce55ad5374c1d6af49b3
borisngong <borisngong@gmail>
2024-10-15 18:31:23 -0700

    Calculate the time recorded in asyncio

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-""""
-Module for working with time of execution in async
+"""
+Module for working with time of execution in async tasks.
 """
 import asyncio
 import time
@@ -9,14 +9,9 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime():
-    """Function responsible for returning the toal runtime of asyncio """
+    """Function responsible for returning the total runtime of asyncio tasks."""
     startTime = time.perf_counter()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     endTime = time.perf_counter()
     recordTime = endTime - startTime
     return recordTime
