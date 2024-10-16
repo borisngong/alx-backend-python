e22341a8189f48648a067c49ec30e79eed2ab7a9
borisngong <borisngong@gmail>
2024-10-15 18:00:44 -0700

    Async Generator

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,11 @@
 """Module for working with Async Comprehensions"""
 
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """Function responsible for returning comprehensive list """
     comprehensive = [k async for k in async_generator()]
     return comprehensive
