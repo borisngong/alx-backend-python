4a594da088e61f9fd21c18c5478ea07cce7aea7c
borisngong <borisngong@gmail>
2024-11-04 08:19:47 -0800

    Unittest

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -56,27 +56,16 @@ class TestMemoize(unittest.TestCase):
         """
         class TestClass:
             """Test Class for wrapping with memoize """
-
+            
             def a_method(self):
                 return 42
 
             @memoize
             def a_property(self):
                 return self.a_method()
-        with patch.object(TestClass, 'a_method',
-                          return_value=42) as mock_method:
-            test_instance = TestClass()
-            # Call a_property twice
-            result1 = test_instance.a_property
-            result2 = test_instance.a_property
 
-            # Assertions
-            self.assertEqual(result1, 42)
-            self.assertEqual(result2, 42)
-
-            # Ensure that a_method was only called once
-            mock_method.assert_called_once()
-
-
-if __name__ == '__main__':
-    unittest.main()
+        with patch.object(TestClass, 'a_method') as mock:
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+            mock.assert_called_once()
